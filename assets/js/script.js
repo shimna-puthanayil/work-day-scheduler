2b94b12ade7adb2912b4b8f215ed44bd8c3af981
shimna <chimmu.p@gmail.com>
2023-06-22 16:20:18 +1000

    displayed events in the corresponding time blocks in the planner

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,35 +3,38 @@
 // in the html.
 $(function () {
     var dayDisplayEl = $('#currentDay');
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    function saveEventsToStorage()
-    {
-        var eventId=$(this).parent('div').attr('id');
-        var eventText=$(this).parent().children().eq(1).val();
-        
-        localStorage.setItem(eventId,eventText);
+    
+    function saveEventsToStorage() {
+        var eventId = $(this).parent('div').attr('id');
+        var eventText = $(this).parent().children().eq(1).val();
+
+        localStorage.setItem(eventId, eventText);
+        $('#message').text("Appointment added to local storage .");
+        $('#icon').attr('style','visibility:visible');
     }
+    //  Added a listener for click events on the save button. 
     var mainDivEl = $('#time-block-display');
-    mainDivEl.on('click','.saveBtn',saveEventsToStorage);
-    //
+    mainDivEl.on('click', '.saveBtn', saveEventsToStorage);
+
+    
+    
+    //Display color coded  time blocks according to the current time .
     var currentHour = dayjs().format('H');
-   
+    
     var divElements = mainDivEl.children('div');
     for (var i = 0; i < divElements.length; i++) {
         var time = divElements.eq(i).attr('id');
         var timeOfBlock = time.substring(5, 7);
-        // if (timeOfBlock.isBefore(currentHour)) {
+       
         if (parseInt(timeOfBlock) > currentHour) {
+            // if ((dayjs().format('H')).isBefore(timeOfBlock)) {
             mainDivEl.children('div').eq(i).addClass('future');
         }
+        // else if (dayjs().isAfter(timeOfBlock)) {
         else if (parseInt(timeOfBlock) < currentHour) {
             mainDivEl.children('div').eq(i).addClass('past');
         }
+
         else {
             mainDivEl.children('div').eq(i).addClass('present');
         }
@@ -41,6 +44,17 @@ $(function () {
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
     //
+    for (var i = 0; i < divElements.length; i++) {
+        var eventId = divElements.eq(i).attr('id');
+        var eventText=localStorage.getItem(eventId);
+        if(eventText!=""&&eventText!=null)
+        {
+            divElements.eq(i).children(1).val(eventText);   
+        }
+       
+        
+    }
+   
     // TODO: Add code to display the current date in the header of the page.
     var currentDay = dayjs().format('dddd, MMMM D, YYYY');
     dayDisplayEl.text(currentDay);
