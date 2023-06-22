146413077dfc6ed9c138c748c2b331991e839c73
shimna <chimmu.p@gmail.com>
2023-06-22 17:35:32 +1000

    added success alert after save

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,48 +2,56 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+    var messageTimerCount = 2;
     var dayDisplayEl = $('#currentDay');
-    
+    function successMessageTimer() {
+        // Sets timer
+        successTimer = setInterval(function () {
+          if (messageTimerCount === 0) {
+            // Clears interval and stops timer
+            clearInterval(successTimer);
+            $('#message').text("");
+            $('#icon').addClass('invisible');
+          }
+          else {
+            messageTimerCount--;
+          }
+        }, 1000);
+      }
     function saveEventsToStorage() {
         var eventId = $(this).parent('div').attr('id');
         var eventText = $(this).parent().children().eq(1).val();
 
         localStorage.setItem(eventId, eventText);
         $('#message').text("Appointment added to local storage .");
-        $('#icon').attr('style','visibility:visible');
+        $('#icon').removeClass('invisible');
+        $('#icon').addClass('visible');
+        successMessageTimer();
     }
     //  Added a listener for click events on the save button. 
     var mainDivEl = $('#time-block-display');
-    mainDivEl.on('click', '.saveBtn', saveEventsToStorage);
-
-    
+    mainDivEl.on('click', '.saveBtn', saveEventsToStorage); 
     
     //Display color coded  time blocks according to the current time .
     var currentHour = dayjs().format('H');
-    
-    var divElements = mainDivEl.children('div');
+  var divElements = mainDivEl.children('div');
     for (var i = 0; i < divElements.length; i++) {
         var time = divElements.eq(i).attr('id');
         var timeOfBlock = time.substring(5, 7);
        
         if (parseInt(timeOfBlock) > currentHour) {
-            // if ((dayjs().format('H')).isBefore(timeOfBlock)) {
+            
             mainDivEl.children('div').eq(i).addClass('future');
         }
-        // else if (dayjs().isAfter(timeOfBlock)) {
         else if (parseInt(timeOfBlock) < currentHour) {
             mainDivEl.children('div').eq(i).addClass('past');
         }
-
         else {
             mainDivEl.children('div').eq(i).addClass('present');
         }
     }
 
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
+    //Get events from local storage and displays to corresponding time blocks.
     for (var i = 0; i < divElements.length; i++) {
         var eventId = divElements.eq(i).attr('id');
         var eventText=localStorage.getItem(eventId);
